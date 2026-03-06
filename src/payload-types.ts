8da60b48072bfcd9e6660c3c5900e51aa78f526d
Cris Lucero <lucerocris22@gmail.com>
2026-03-06 15:17:06 -0800

    feat: linked DO spaces storage

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -158,6 +158,7 @@ export interface User {
 export interface Media {
   id: number;
   alt: string;
+  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -369,6 +370,7 @@ export interface UsersSelect<T extends boolean = true> {
  */
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
+  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
 import { Users } from './collections/Users'
+import { s3Storage } from '@payloadcms/storage-s3'
 import { Media } from './collections/Media'
 import { Inquiries } from '@/collections/Inquiries'
 import { Cocktails } from '@/collections/Cocktails'
@@ -34,5 +35,22 @@ export default buildConfig({
     },
   }),
   sharp,
-  plugins: [],
+    plugins: [
+    s3Storage({
+      collections: {
+        media: {
+          prefix: 'tipsytrails',
+        }, 
+      },
+      bucket: process.env.DO_SPACE_NAME!,
+      config: {
+        endpoint: process.env.DO_SPACE_ENDPOINT,
+        credentials: {
+          accessKeyId: process.env.DO_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.DO_SECRET_ACCESS_KEY!,
+        },
+        region: process.env.DO_SPACE_REGION,
+      },
+    }),
+  ],
 })
